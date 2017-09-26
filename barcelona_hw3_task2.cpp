36203b459fd52be664d554dd5b5b92f02fd09dd6
jaxongreenwood <jaxongreenwood@weber.mail.edu>
2017-09-26 13:47:49 -0600

    Added .gitignore + 2 misc files Added Task 2 to CMake

--- barcelona_hw3_task2.cpp
+++ barcelona_hw3_task2.cpp
@@ -3,9 +3,34 @@
 // Copyright (c) 2017 WSU
 //
 #include <iostream>
-
 using namespace std;
 
+class serial {
+private:
+
+    int serNum;
+    static int count;
+
+public:
+
+    serial(): serNum(0){count++;};
+
+
+    int getCount() {
+        return count;
+    }
+
+    void showSerial() {
+
+        cout << "serial number is = " << serNum << endl;
+
+    }
+
+    serial setSerial();
+
+};
+
+int serial::count = 0;
 // Constants
 
 // Prototypes
@@ -14,7 +39,22 @@ using namespace std;
 
 int main(void) {
 
+    serial alpha;      //objects
+
+    alpha.setSerial();
+
+    alpha.showSerial();
+
+
+
     return 0;
 }
 
 // Function Definitions
+serial serial::setSerial() {
+
+    serial sn1;
+    sn1.serNum = sn1.count;
+
+    return sn1;
+}
