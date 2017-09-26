44475cd02dfafecb3d12ea12c76918a5d0faa649
jaxongreenwood <jaxongreenwood@weber.mail.edu>
2017-09-26 12:54:19 -0600

    Initial commit

---
+++ barcelona_hw3_task2.cpp
@@ -0,0 +1,23 @@
+
+// File: barcelona_hw3_task2
+// Created by Jaxon Greenwood on 9/26/2017 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+
+// Constants
+
+// Prototypes
+
+// Main Program Program
+
+int main(void) {
+
+    $END$
+
+    return 0;
+}
+
+// Function Definitions
