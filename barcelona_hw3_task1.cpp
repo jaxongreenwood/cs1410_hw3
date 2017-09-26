e2c0034426798cce4a1f4652fbdf1efe3aff1dea
Carson Tyler <carsontyler@mail.weber.edu>
2017-09-26 13:15:00 -0600

    Added class

--- barcelona_hw3_task1.cpp
+++ barcelona_hw3_task1.cpp
@@ -2,11 +2,21 @@
 // Created by Jaxon Greenwood, Carson Tyler on 9/26/2017 for CS1410.
 // Copyright (c) 2017 WSU
 #include <iostream>
-
 using namespace std;
 
-// Constants
+// Class
 
+class Distance
+{
+private:
+    int month;
+    int day;
+    int year;
+
+public:
+    // Constructor
+
+};
 // Prototypes
 
 // Main Program Program
