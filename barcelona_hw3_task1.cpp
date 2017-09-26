c4b4bf066a684a77b010bbb97c5564f8d04f99cd
Carson Tyler <carsontyler@mail.weber.edu>
2017-09-26 13:07:29 -0600

    Fixed

---
+++ barcelona_hw3_task1.cpp
@@ -0,0 +1,19 @@
+// File: barcelona_hw3_task1
+// Created by Jaxon Greenwood, Carson Tyler on 9/26/2017 for CS1410.
+// Copyright (c) 2017 WSU
+#include <iostream>
+
+using namespace std;
+
+// Constants
+
+// Prototypes
+
+// Main Program Program
+
+int main(void) {
+
+    return 0;
+}
+
+// Function Definitions
